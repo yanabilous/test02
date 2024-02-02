02d4afdc071a77dc9f972a2955963ff80a6748e2
yanabilous <yanabilous05@gmail.com>
2024-02-02 20:50:49 +0200

    init

---
+++ test-anime/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import TestPage from "./pages/TestPage.jsx";
+
+function App() {
+
+
+  return (
+    <>
+      <TestPage/>
+    </>
+  );
+}
+
+export default App;
---
+++ test-anime/src/pages/TestPage.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header.jsx";
+
+function TestPage(){
+  return(
+    <>
+      <Header/>
+    </>
+  )
+
+}
+export default TestPage;
